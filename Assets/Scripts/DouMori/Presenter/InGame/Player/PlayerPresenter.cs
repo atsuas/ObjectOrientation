5cf18832b4871a4ee22b9505762ec87c9c64b0a7
AtsushiKinoshita <atsushi.kinoshita@translimit.co.jp>
2022-04-02 19:40:55 +0900

    Player: キャラクターコントローラーを使用、重力をつける

--- Assets/Scripts/DouMori/Presenter/InGame/Player/PlayerPresenter.cs
+++ Assets/Scripts/DouMori/Presenter/InGame/Player/PlayerPresenter.cs
@@ -6,8 +6,11 @@ public class PlayerPresenter : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
+    public float gravityScale = 5f; // プラスする重力値
 
     private Vector3 moveDirection;
+
+    public CharacterController charController;
 
     void Start()
     {
@@ -16,12 +19,22 @@ public class PlayerPresenter : MonoBehaviour
 
     void Update()
     {
+        // 移動させる
+        float yStore = moveDirection.y;
         moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveDirection = moveDirection * moveSpeed;
+        moveDirection.y = yStore;
 
         if (Input.GetButtonDown("Jump"))
         {
             moveDirection.y = jumpForce;
         }
-        transform.position = transform.position + (moveDirection * Time.deltaTime * moveSpeed);
+
+        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+
+        // transform.position = transform.position + (moveDirection * Time.deltaTime * moveSpeed);
+
+        // キャラクターコントローラーのMove関数で動かす
+        charController.Move(moveDirection * Time.deltaTime);
     }
 }
